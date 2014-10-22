058bc879ccdc189f2c166a77d23e5355668984af
Retl <carlis.moore@gmail.com>
2014-10-22 13:27:52 -0400

    Generated the Template
    Got the template generated and it works so far when the server is
    launched. Was able to get to the Welcome page and everything.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ba9cd09963f9d560a61786326c4899bb3f1ab15a45937a9cdeee1807403551b820cf075262a3168b78994e0010bd98e874089aa215c15c14273270596f5c5c14'
